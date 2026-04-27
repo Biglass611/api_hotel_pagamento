1bce369f20f2d53e82384088994610d141c0e739
Lucas Nunes <lucas.n.santos2002@hotmail.com>
2026-04-27 12:01:40 -0300

    Primeiro commit

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const restify = require("restify");
+const UsuariosController = require("./controllers/usuarios.controller");
+const server = restify.createServer({
+ name: "api-usuarios-restify"
+});
+
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
+server.get("/usuarios", UsuariosController.listar);
+server.post("/usuarios", UsuariosController.criar);
+const PORT = 3000;
+server.listen(PORT, () => {
+ console.log(`${server.name} rodando em ${server.url}`);
+});
+
